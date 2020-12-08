c6b744e69455a3c5a46232ef01345da227f8e98f
henchan13 <henchanlee@gmail.com>
2020-12-08 09:39:21 +0900

    오타 수정

--- run.py
+++ run.py
@@ -10,5 +10,5 @@ def home():
 # run.py가 엔트리포인트인 경우에만 작동
 # 리눅스 서벅로 가면 wsgi.py이 엔트리 포인트 이므로, 작동안함
 # 페브릭으리 설정한 룰에 의해서 서버가 작동된다
-if __name__ ='__main__'
+if __name__=='__main__':
     app.run(debug=True)
